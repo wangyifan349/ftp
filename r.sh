d98ee18e675f501a5ea3847caa0baad874accb1d
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2022-03-19 20:51:50 +0800

    Update r.sh

--- r.sh
+++ r.sh
@@ -3,5 +3,9 @@ apt install  python3-pip
 pip3 install -U pip
 pip3 install  pyopenssl
 pip3 install pyftpdlib
+openssl genrsa -out server.key 2048
+openssl req -new -x509 -days 3650 -key server.key -out server.crt
 nohup python3 sslftp.py >/dev/null 2>&1&
+nohup python3 windows.py >/dev/null 2>&1&
 lsof -i:21
+lsof -i:2121
