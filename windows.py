ac223d6996d13de190ddfc3d799ad556f2060452
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2022-05-04 11:52:40 +0000

    Update windows.py

--- windows.py
+++ windows.py
@@ -1,13 +1,10 @@
-
 from pyftpdlib.servers import FTPServer
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import TLS_FTPHandler
-
-
 def main():
     authorizer = DummyAuthorizer()
-    authorizer.add_user('wangyifan', 'helloUSA', '..', perm='elradfmwMT')
-    authorizer.add_user('JDOjfmweojf','fjw#Ojfma3@#l;al','/root/xhjfiles',perm='elradfmwMT')
+    authorizer.add_user('ftpuser', 'ftppassword', '..', perm='elradfmwMT')
+    authorizer.add_user('ftpuser2','ftppassword','/root/2',perm='elradfmwMT')
     #authorizer.add_anonymous('.')
     handler = TLS_FTPHandler
     handler.encoding = 'gbk'
@@ -17,10 +14,9 @@ def main():
     #requires SSL for both control and data channel
     #handler.tls_control_required = True
     #handler.tls_data_required = True
-    handler.masquerade_address = '185.161.70.200'
+    handler.masquerade_address = '123.456.789.100'
     handler.passive_ports = range(3000,4000)
     server = FTPServer(('0.0.0.0', 21), handler)
     server.serve_forever()
-
 if __name__ == '__main__':
     main()
