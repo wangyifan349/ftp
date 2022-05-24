f93fd7b37c3aac511993481da629a5176abaea06
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2022-05-24 10:24:35 +0000

    Update sslftp.py

--- sslftp.py
+++ sslftp.py
@@ -4,7 +4,7 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import TLS_FTPHandler
 def main():
     authorizer = DummyAuthorizer()
-    authorizer.add_user('ftpuser', 'ftppassword', '..', perm='elradfmwMT')
+    authorizer.add_user('wangyifan', 'helloUSA', '..', perm='elradfmwMT')
     #authorizer.add_anonymous('.')
     handler = TLS_FTPHandler
     handler.certfile = 'server.crt'
@@ -13,7 +13,7 @@ def main():
     #requires SSL for both control and data channel
     handler.tls_control_required = True
     handler.tls_data_required = True
-    handler.masquerade_address = '123.456.789.200'
+    handler.masquerade_address = '185.161.70.200'
     handler.passive_ports = range(1000,2500)
     server = FTPServer(('0.0.0.0', 2121), handler)
     server.serve_forever()
