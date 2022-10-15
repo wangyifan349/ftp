778be275558fb3c2a1806929ebc122e81db0937e
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2022-10-15 18:56:32 +0800

    Update windows.py

--- windows.py
+++ windows.py
@@ -3,18 +3,19 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import TLS_FTPHandler
 def main():
     authorizer = DummyAuthorizer()
-    authorizer.add_user('wangyifan', 'helloUSA', '..', perm='elradfmwMT')
-    authorizer.add_user('wangyifan2','helloUSA','/home',perm='elradfmwMT')
+    authorizer.add_user('wangyifan', 'hellowindows123', '..', perm='elradfmwMT')
     #authorizer.add_anonymous('.')
     handler = TLS_FTPHandler
-    handler.encoding = 'gbk'
+    handler.encoding = 'gbk'#windows下是GBK编码的真是奇了啪.....
     handler.certfile = 'server.crt'
     handler.keyfile = 'server.key'
     handler.authorizer = authorizer
     #requires SSL for both control and data channel
     handler.tls_control_required = False
     handler.tls_data_required = False
-    #handler.masquerade_address = '185.161.70.200'
+    #windows的资源管理器实在是垃圾的FTP客户端,他不允许使用以上两个TLS功能,这会导致FTP的命令部分和传文件部分全部暴漏,很不隐私。
+    #如把上面两个改成True的话,windows客户端(资源管理器)将无法工作。
+    #handler.masquerade_address = '185.161.70.200'#windows资源管理器下不能开启这个!!!!!
     handler.passive_ports = range(3000,4000)
     server = FTPServer(('0.0.0.0', 21), handler)
     server.serve_forever()
